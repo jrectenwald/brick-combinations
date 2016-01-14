e9212e5ea19c306fd2de0215c79c698c448b2f62
jrectenwald <jimrec101@gmail.com>
2016-01-14 15:27:42 -0500

    refactor all possible rows

--- brick.rb
+++ brick.rb
@@ -1,40 +1,48 @@
 class Wall
 
-  def brick_combinations(width, height)
-    
+  def relative_amounts(width)
+    amounts = []
+    iterate_three_ft_count(width, amounts)
+    return amounts
   end
 
-  def ratios(width, height)
-    arr = []
-    (0..width).each do |i|
-      (0..width).each do |j|
-        if i == (-1.5 * j + width / 3)
-          arr.push({four_and_half_ft: j, three_ft: i})
-        end
-      end
+  def iterate_three_ft_count(width, amounts)
+    (0..(width/3)).each do |i|
+      iterate_four_and_half_ft_count(i, width, amounts)
     end
-    return arr
+  end
+
+  def iterate_four_and_half_ft_count(i, width, amounts)
+    (0..(width/3)).each do |j|
+      push_to_array(i, j, width, amounts)
+    end
+  end
+
+  def push_to_array(i, j, width, amounts)
+    if combination_matches_width(i, j, width)
+      amounts.push({'4.5': j, '3': i})
+    end
+  end
+ 
+  def combination_matches_width(three_ft_bricks, four_and_half_ft_bricks, wall_width)
+    three_ft_bricks == (-1.5 * four_and_half_ft_bricks + wall_width / 3)
   end
 
   def row_combination(bricks)
-    array = []
-    bricks.values[0].times do
-      array.push("4.5")
+    bricks.each_with_object([]) do |(length, frequency), arr|
+      frequency.times {arr.push(length.to_s.to_i)}
     end
-    bricks.values[1].times do
-      array.push("3")
-    end
-    return array
   end
 
-  def all_possible_rows(all_ratios)
-    all_ratios.each do |hash|
-      
+  def all_possible_rows(brick_amounts)
+    brick_amounts.each_with_object([]) do |bricks, arr|
+      one_row_permutations(bricks).each {|combination| arr.push(combination)}
     end
   end
 
   def one_row_permutations(bricks)
     arr = row_combination(bricks)
-    arr.permutation(arr.count).to_a.uniq
+    length = arr.length
+    arr.permutation(length).to_a.uniq
   end
 end
