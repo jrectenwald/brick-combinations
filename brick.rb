48b57b945f108d1a9c43cae09f28db2e7a14ff3c
jrectenwald <jimrec101@gmail.com>
2016-01-20 20:30:32 -0500

    refactor build_higher_levels

--- brick.rb
+++ brick.rb
@@ -1,11 +1,10 @@
-gem 'pry'
-require 'pry'
-
 class Wall
-  attr_accessor :count
-
-  def initialize
-    @count = Count.new
+  def compare_rows(row, row_above, similarities=0)
+    row.each do |brick| 
+      similarities += 1 if row_above.include?(brick) 
+      break if similarities == 1
+    end
+    similarities
   end
 
   def one_row_possibilities(brick_lengths, width, walls=[], partial_wall=[])
@@ -18,55 +17,45 @@ class Wall
         one_row_possibilities(brick_lengths, width, walls, next_guess)
       end
     end
-    return walls
-  end
-
-  def compare_rows(row, row_above)
-    similarities = 0
-    row.each do |brick| 
-      similarities += 1 if row_above.include?(brick) 
-      break if similarities == 1
-    end
-    return similarities
-  end
-
-  def build_higher_levels(row, matchups_hash, height, current_height)
-    matchups_hash[row].each do |row_above|
-      if height == current_height
-        self.count.count += 1
-      else
-        new_height = current_height + 1
-        all_rows = build_higher_levels(row_above, matchups_hash, height, new_height)
-      end
-    end
+    walls
   end
 
   def matchups(all_rows)
-    matchups_hash = {}
-    all_rows.each do |row_below|
-      matches_to_bottom_row = 0
+    all_rows.each_with_object({}) do |row_below, matchups_hash|
       all_rows.each do |row_above|
         similarities = compare_rows(row_below, row_above)
         if similarities == 0
-          matchups_hash.has_key?(row_below) ? matchups_hash[row_below] << row_above : matchups_hash[row_below] = [row_above]
+          matchups_hash.has_key?(row_below) ? matchups_hash[row_below].push(row_above) : matchups_hash[row_below] = [row_above]
         end
       end
     end
-    return matchups_hash
   end
 
-  def wall_combination_count(width, height)
+  def build_higher_levels(row, matchups_hash, height, current_height, count=0)
+    matchups_hash[row].each do |row_above|
+      if (height - 1) == current_height
+        count += matchups_hash[row_above].count
+      else
+        new_height = current_height + 1
+        count += build_higher_levels(row_above, matchups_hash, height, new_height)
+      end
+    end
+    count
+  end
+
+  def two_foot_wall(matchups_hash, count)
+    matchups_hash.each do |row_below, compatible_rows| 
+      count += compatible_rows.count
+    end
+    count
+  end
+  
+  def wall_combination_count(width, height, count=0)
     all_rows = one_row_possibilities([3.0, 4.5], width, [], [0])
     return all_rows.count if height == 1
     matchups_hash = matchups(all_rows)
-    all_rows.each {|row| build_higher_levels(row, matchups_hash, height, 2)}
-    return self.count.count
-  end
-end
-
-class Count
-  attr_accessor :count
-  def initialize
-    @count = 0
+    return two_foot_wall(matchups_hash, count) if height == 2
+    all_rows.each {|row| count += build_higher_levels(row, matchups_hash, height, 2)}
+    count
   end
 end
